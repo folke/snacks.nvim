089b812afa1d7071484533d6234cba463574c7a8
Linus Boehm <linusboehm@gmail.com>
2024-11-21 14:15:32 -0500

    fix: use fs_stat instead of two checks

--- lua/snacks/git.lua
+++ lua/snacks/git.lua
@@ -20,7 +20,7 @@ function M.get_root(path)
   path = path == "" and (vim.uv or vim.loop).cwd() or path
   local git_root ---@type string?
   for dir in vim.fs.parents(path) do
-    if vim.fn.isdirectory(dir .. "/.git") == 1 or vim.fn.filereadable(dir .. "/.git") == 1 then
+    if vim.uv.fs_stat(dir .. "/.git") ~= nil then
       git_root = dir
       break
     end
