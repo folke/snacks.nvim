2d6e9493dfae6c28ab6db1f483838a0b520e3a25
doeke <doekebuursam@gmail.com>
2025-11-26 00:58:54 -0500

    Check that formatted properties exist.

--- lua/snacks/picker/format.lua
+++ lua/snacks/picker/format.lua
@@ -433,12 +433,16 @@ function M.tmux_panes(item)
       "SnacksPickerIcon",
     }
   end
-  ret[#ret + 1] = { a(item.session_name, 8, { truncate = true, align = "right" }), "SnacksPickerIdx" }
-  ret[#ret + 1] = { " :", "SnacksPickerDelim" }
-  ret[#ret + 1] = { a(item.window_index, 3, { align = "center" }), "SnacksPickerIdx" }
-  ret[#ret + 1] = { ". ", "SnacksPickerDelim" }
-  ret[#ret + 1] = { a(item.pane_index, 3), "SnacksPickerIdx" }
-  ret[#ret + 1] = { item.current_command, "SnacksPickerCode" }
+  if item.session_name and item.window_index and item.pane_index then
+    ret[#ret + 1] = { a(item.session_name, 8, { truncate = true, align = "right" }), "SnacksPickerIdx" }
+    ret[#ret + 1] = { " :", "SnacksPickerDelim" }
+    ret[#ret + 1] = { a(item.window_index, 3, { align = "center" }), "SnacksPickerIdx" }
+    ret[#ret + 1] = { ". ", "SnacksPickerDelim" }
+    ret[#ret + 1] = { a(item.pane_index, 3), "SnacksPickerIdx" }
+  end
+  if item.current_command then
+    ret[#ret + 1] = { item.current_command, "SnacksPickerCmd" }
+  end
   if item.pane_active then
     ret[#ret + 1] = { " (active)", "SnacksPickerComment" }
   end
